ba3682b4db4d9ff7b9da85723f3362ca950bf6e8
Farih06 <workwithfarih@gmail.com>
2026-05-05 16:22:08 +0530

    first program

---
+++ src/vairables.java
@@ -0,0 +1,14 @@
+public class vairables {
+    static void main() {
+
+
+        int age;
+        age = 10;
+
+        System.out.println(age);
+
+        int totalMarks= 20;
+        System.out.println(totalMarks);
+
+    }
+}
