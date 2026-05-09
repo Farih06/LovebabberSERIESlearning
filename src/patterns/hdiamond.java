eaedf85f05f4884cea832e78b10d8e2f888b2d7f
Farih06 <workwithfarih@gmail.com>
2026-05-09 21:37:59 +0530

    day5

---
+++ src/patterns/hdiamond.java
@@ -0,0 +1,31 @@
+package patterns;
+
+public class hdiamond {
+    static void main() {
+        int n = 4;
+
+        for (int i = 1; i<= n; i++) {
+            //part 1
+            for (int j =1;j<=i;j++ ){
+                System.out.print("* ");
+            }
+
+            //part 2
+            for(int j=1;j<=(2*(n-i));j++){
+                System.out.print("  ");
+            }
+            //part 3
+            for (int j =1;j<=i;j++ ){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        //part 4
+        for(int i=1;i<=n;i++){
+            for(int j=n;j<=1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
